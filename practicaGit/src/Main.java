253f07452f5409a17eb89a8244bb9fabd8f31e01
PauGuiot15 <pguiot15@elpuig.xeill.net>
2024-04-09 22:07:34 +0200

    palo

--- practicaGit/src/Main.java
+++ practicaGit/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("dfgsdfsdfçla a todos!");
+        System.out.println("dfgsdfsdfçla a todos!");
+        System.out.println("dfgsdfsdfçla a todos!");
+        System.out.println("dfgsdfsdfçla a todos!");
+
     }
 }
